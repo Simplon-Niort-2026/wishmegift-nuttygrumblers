e2727806388a9966dc6921eeecade45bba857faf
Ludovic Thibaud <164179276+Ludo-dev-1@users.noreply.github.com>
2026-04-27 14:49:04 +0200

    Update src/main/java/co/simplon/wishmegift/entity/User.java
    Co-authored-by: GOMES Mathéo <147269298+gomes-matheo@users.noreply.github.com>

--- src/main/java/co/simplon/wishmegift/entity/User.java
+++ src/main/java/co/simplon/wishmegift/entity/User.java
@@ -20,7 +20,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Integer id;
 
     @Column(name = "lastname", nullable = false)
     @Nonnull
