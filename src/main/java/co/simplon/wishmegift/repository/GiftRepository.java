9f9275a14d61fa85af78e20a37798c188c6c1732
Ludovic Thibaud <164179276+Ludo-dev-1@users.noreply.github.com>
2026-04-29 15:15:01 +0200

    feat : création du giftService (#39)
    * feat : création du giftService
    
    * fix : findById
    
    ---------
    
    Co-authored-by: THIBAUD Ludovic <ludovic.thibaud@maif.fr>

--- src/main/java/co/simplon/wishmegift/repository/GiftRepository.java
+++ src/main/java/co/simplon/wishmegift/repository/GiftRepository.java
@@ -2,7 +2,6 @@ package co.simplon.wishmegift.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import co.simplon.wishmegift.entity.Gifts;
 
 @Repository
---
+++ src/main/java/co/simplon/wishmegift/service/GiftService.java
@@ -0,0 +1,29 @@
+package co.simplon.wishmegift.service;
+
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import co.simplon.wishmegift.entity.Gifts;
+import co.simplon.wishmegift.repository.GiftRepository;
+
+@Service
+public class GiftService {
+
+    private GiftRepository giftRepository;
+
+    public GiftService(GiftRepository giftRepositoryInjected) {
+        this.giftRepository = giftRepositoryInjected;
+    }
+
+    public Gifts create(Gifts gift) {
+        return this.giftRepository.save(gift);
+    }
+
+    public Optional<Gifts> findById(final Long id) {
+        return this.giftRepository.findById(id);
+    }
+
+    public void deleteGiftById(final Long id) {
+        this.giftRepository.deleteById(id);
+    }
+
+}
