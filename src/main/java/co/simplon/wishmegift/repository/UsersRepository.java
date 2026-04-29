e675136650730ef641f219b3395a42d1004d7d96
GUIBERT Vincent <vincent.guibert@externe.maif.fr>
2026-04-29 16:50:25 +0200

    feat : ajout du service user + securisation password

--- src/main/java/co/simplon/wishmegift/repository/UsersRepository.java
+++ src/main/java/co/simplon/wishmegift/repository/UsersRepository.java
@@ -4,4 +4,6 @@ import co.simplon.wishmegift.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
+
+    User findUserById(Long id);
 }
